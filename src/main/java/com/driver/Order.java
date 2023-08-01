3a409b42843efd17a55044f249e591b6bb40bf80
VAibhAV <ashishrambsp0077@gmail.com>
2023-08-01 22:45:15 +0530

    Test@2

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -10,18 +10,18 @@ public class Order {
         this.deliveryTime = convertDeliveryTimeToMinutes(deliveryTime);
     }
 
-    private int convertDeliveryTimeToMinutes(String deliveryTime) {
-        String[] timeParts = deliveryTime.split(":");
-        int hours = Integer.parseInt(timeParts[0]);
-        int minutes = Integer.parseInt(timeParts[1]);
-        return hours * 60 + minutes;
-    }
-
     public String getOrderId() {
         return orderId;
     }
 
     public int getDeliveryTime() {
         return deliveryTime;
+    }
+
+    private int convertDeliveryTimeToMinutes(String deliveryTime) {
+        String[] timeParts = deliveryTime.split(":");
+        int hours = Integer.parseInt(timeParts[0]);
+        int minutes = Integer.parseInt(timeParts[1]);
+        return hours * 60 + minutes;
     }
 }
